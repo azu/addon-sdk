0489ef3566cc7edf048d01c71cb11464bf1a5df7
Erik Vold <erikvvold@gmail.com>
2013-04-21 19:56:58 -0700

    Adding chrome-mod module from https://github.com/ochameau/tree-tabs/tree/0e8dc0ea8636a0ea0810ff1fdba70bc6490a3002

---
+++ packages/pathfinder/tests/test-chrome-mod.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { Ci, Cc } = require("chrome");
+const { ChromeMod } = require("chrome-mod");
+const { windowIterator } = require("api-utils/window-utils");
+
+/* Tests for the ChromeMod APIs */
+
+exports.testChromeMod = function(test) {
+  test.waitUntilDone();
+
+  let chromeMod = new ChromeMod({
+    type: "navigator:browser",
+
+    contentScript: 'new ' + function WorkerScope() {
+      document.documentElement.setAttribute("chrome-mod-ok", "true");
+      self.on("message", function (data) {
+        if (data=="hi")
+          self.postMessage("bye");
+      });
+    },
+
+    onAttach: function(worker) {
+      worker.on("message", function (data) {
+        test.assertEqual(data, "bye", "get message from content script");
+        // Search for this modified window
+        for(let win in windowIterator()) {
+          if (win.document.documentElement.getAttribute("chrome-mod-ok") == "true") {
+            test.done();
+            return;
+          }
+        }
+        test.fail("Unable to found the modified window, with 'chrome-mod-ok' attribute");
+      });
+      worker.postMessage("hi");
+    }
+  });
+};
